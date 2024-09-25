5cd28cd34c17df4509139310fea79d5381d560c0
Clairtonlm <Clairtonlm@gmail.com>
2024-09-25 00:27:19 -0300

    CRUD

--- Api-Node-MF/index.js
+++ Api-Node-MF/index.js
@@ -1,11 +1,44 @@
 import express from 'express'
+//const express = require('express')
 
 const app = express()
 app.use(express.json())
 
+//CRUD -> cREATE, READ, UPDATE, DELETE
+const cursos = ['Node Js', 'Java', 'PHP'];
+
+// rotas para todos os cursos
+app.get('/cursos', (req, res)=>{
+    return res.json(cursos);
+})
 // Primeiras rotas
-app.get('/curso', (req, res)=>{
-    res.status(200).json(req.body)
+app.get('/cursoS/:index', (req, res)=>{
+    const {index} = req.params;
+
+    return res.json(cursos [index])
+})
+//Criaar curos
+app.post('/cursos', (req, res)=>{
+    const {name} = req.body
+    cursos.push(name)
+    return res.json(cursos);
+})
+//Atualizando cursos
+app.put('/cursos/:index', (req, res)=>{
+    const {index} = req.params;
+    const {name} = req.body;
+
+    cursos[index] = name;
+    return res.json(cursos);
+
+})
+//delete
+app.delete('/cursos/:index', (req, res)=>{
+    const {index} = req.params;
+
+    cursos.splice(index, 1)
+    return res.json({message: "Deletado"});
+
 })
 
 app.listen(3000)
